df5a002ab28d7996d026db41ce3032765633034d
Muhammad Ilham <ilhamthetamvan@gmail.com>
2025-08-13 09:53:28 +0800

    fsdfs

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrationsmHSBDhjas djash.
      */
     public function up(): void
     {
